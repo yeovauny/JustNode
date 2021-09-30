7d608ac3ddc7b25184a2e50578fc95fe2d29c3ce
Eduardo Yeovauny Contreras Dilluvio <eduardo.contreras_ex@scotiabank.cl>
2021-09-30 16:57:17 -0300

    express simple example

--- node-express/index.js
+++ node-express/index.js
@@ -2,12 +2,45 @@ const { Console } = require('console');
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost';
 const port = 3000;
 
 const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.all('/dishes',(req,res,next)=>{
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    res.end('I send all dishes to you');
+    next();
+});
+
+app.get('/dishes/:dishID',(req,res,next) =>{
+    res.end("will send details of the dishes! "+req.params.dishID+ " to you!");
+
+});
+
+app.post('/dishes/:dishID',(req,res,next) =>{
+    console.log("entra aqui");
+    res.end(`will add the dish ${req.body.name} with details ${req.body.description}`);
+});
+
+app.put('/dishes/:dishID',(req,res,next) =>{
+    res.statusCode = 200;
+    res.write(`Will update the dish ${req.params.dishID} `);
+    res.end(`Will update the dish ${req.body.name} with  details ${req.body.description}`)
+});
+
+app.delete('/dishes/:dishID',(req,res,next) =>{
+    res.statusCode = 200;
+    res.end(`Deliting dish ${req.params.dishID}`);
+});
+
+
 app.use(express.static(__dirname+ '/public'))
 
 
