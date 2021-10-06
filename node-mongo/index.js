d49db719c0c556de90b198ce294343e949c52f21
Eduardo Yeovauny Contreras Dilluvio <eduardo.contreras_ex@scotiabank.cl>
2021-10-06 11:29:26 -0300

    new way to comunicate data with database

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -1,9 +1,10 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dboper = require('./operations');
 
 
-const usuario = 'usuario'
-const password = 'pass'
+const usuario = 'courseranodeexample'
+const password = 'xzqhhw4tWwxmwRki'
 
 const url = `mongodb+srv://${usuario}:${password}@cluster0.sy1vg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const dbname = 'myFirstDatabase';
@@ -14,27 +15,34 @@ MongoClient.connect(url, (err,client) =>{
 
     console.log('Connected correctly to server');
     const db = client.db(dbname);
-    const collection = db.collection('dishes');
-    collection.insertOne({"name":"ultrapizza","description":"example description"}, (err,result) => 
-    {
-        assert.equal(err,null);
-        console.log('After Insert: \n');
-        console.log(result.ops);
-
-
-        collection.find().toArray((err,docs)=>{
+    
    
-            assert.equal(err,null);
-            console.log('Found: \n');
-            console.log(docs);
 
-            db.dropCollection("dishes", (err, result) => {
-                assert.equal(err,null);
+   
+    dboper.insertDocument(db,{name:"Eduardo", description:"Testing third"}, 'dishes',(result) =>{
+      
+      console.log('Was Insert Document'+result.ops);
 
-                client.close();
+      dboper.findDocuments(db,'dishes',(docs)=>{
+          console.log(`Found Documents ${docs}`)
+
+          dboper.updateDocument(db,{name:'Eduardo'},{description:"Updated Testing"},'dishes',(result) =>{
+
+            console.log(`Updated document ${result.result}`);
+            dboper.findDocuments(db,'dishes',(docs)=>{
+                console.log(`Found Documents ${docs}`)
+
+                
+                db.dropCollection("dishes", (result) => {
+                    console.log("Dropped Collection: ", result);
+
+                    client.close();
+                });
+
             });
+          })
+      });
 
-        });
     });
        
 });
