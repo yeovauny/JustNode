de57ae8df64fd60fbd7529ad6961d418c2011a23
Eduardo Yeovauny Contreras Dilluvio <eduardo.contreras_ex@scotiabank.cl>
2021-10-19 19:02:46 -0300

    was added authentication after with cookies values on the header

--- conFunsionServer/app.js
+++ conFunsionServer/app.js
@@ -42,29 +42,51 @@ app.use(cookieParser('12345-67890-09876-54321'));
 
 
 function auth(req,res,next){
-  console.log(req.headers);
+  console.log(req.signedCookies);
 
-  var authHeader = req.headers.authorization;
+  if(!req.signedCookies.user){
 
-  if(!authHeader){
-    var err = new Error('Your not autenticated"');
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;
-    return next(err);
+        //when the header don't have the signed cookie, the client needs to autenticate
+          var authHeader = req.headers.authorization;
+
+          if(!authHeader){
+            var err = new Error('Your not autenticated"');
+            res.setHeader('WWW-Authenticate','Basic');
+            err.status = 401;
+            return next(err);
+          }
+        
+          var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+          var username = auth[0];
+          var password = auth[1];
+        
+          if(username === 'admin' && password === 'password' ){
+            res.cookie('user','admin',{signed: true});
+            next();
+          }else{
+            var err = new Error('Your not autenticated"');
+            res.setHeader('WWW-Authenticate','Basic');
+            err.status = 401;
+            return next(err);
+          }  
+
+  } else{
+      // when the header has the signed cookie
+      if(req.signedCookies.user === 'admin'){
+        next();
+      }
+      else{
+        var err = new Error('Your not autenticated"');
+            err.status = 401;
+            return next(err);
+      }
+
+
+
   }
+  
 
-  var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');
-  var username = auth[0];
-  var password = auth[1];
 
-  if(username === 'admin' && password === 'password' ){
-    next();
-  }else{
-    var err = new Error('Your not autenticated"');
-    res.setHeader('WWW-Authenticate','Basic');
-    err.status = 401;
-    return next(err);
-  }  
 }
 
 // autentication
